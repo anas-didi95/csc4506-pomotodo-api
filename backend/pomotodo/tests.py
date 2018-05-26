c31028abb361a235e0eab07d6b48ce8a7499f26b
Anas Juwaidi <anas.didi95@gmail.com>
2018-05-26 11:07:09 +0800

    fixed untracked files

--- backend/pomotodo/tests.py
+++ backend/pomotodo/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from faker import Faker
 
-# Create your tests here.
+class ModelTestCase(TestCase):
+  faker = Faker()
+  title = faker.sentence()
+
+  def setUp(self):
+    self.todo = Todo(title=self.title)
+
+  def testModelTodoCreate(self):
+    old_count = Todo.objects.count()
+    self.todo.save()
+    new_count = Todo.objects.count()
+    self.assertNotEqual(old_count, new_count)
